0d8155bd6bff17fd518961e63d07aff02b5b175d
Siddharth Agarwal <sid.bugzilla@gmail.com>
2011-05-31 14:59:29 -0700

    Fix a couple of things

--- refresh-icons.py
+++ refresh-icons.py
@@ -7,9 +7,9 @@ import ctypes
 from ctypes import wintypes
 
 # http://msdn.microsoft.com/en-us/library/ms644950
-SendMessageTimeout = ctypes.windll.user32.SendMessageTimeout
+SendMessageTimeout = ctypes.windll.user32.SendMessageTimeoutA
 SendMessageTimeout.restype = wintypes.LPARAM  # aka LRESULT
-SendMessageTimeout.argtypes = [wintypes.HWND, wintypes.UINT, ctypes.WPARAM, ctypes.LPARAM,
+SendMessageTimeout.argtypes = [wintypes.HWND, wintypes.UINT, wintypes.WPARAM, wintypes.LPARAM,
                                wintypes.UINT, wintypes.UINT, ctypes.c_void_p]
 
 # http://msdn.microsoft.com/en-us/library/bb762118
@@ -17,7 +17,7 @@ SHChangeNotify = ctypes.windll.shell32.SHChangeNotify
 SHChangeNotify.restype = None
 SHChangeNotify.argtypes = [wintypes.LONG, wintypes.UINT, wintypes.LPCVOID, wintypes.LPCVOID]
 
-HWND_BROADCAST     = 0xffff
+HWND_BROADCAST     = 0xFFFF
 WM_SETTINGCHANGE   = 0x001A
 SMTO_ABORTIFHUNG   = 0x0002
 SHCNE_ASSOCCHANGED = 0x08000000
